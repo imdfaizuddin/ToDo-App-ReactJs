8d217a746f6bf14de0165b07136943b63c427c94
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-02 23:26:14 +0530

    implement add item to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import ListItem from './ListItem';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [item, setItem] = useState([])
+  const [input, setInput] = useState("")
+  function inputValue(event){
+    const {value} = event.target;
+    setInput(value)
+  }
+  function addItem(){
+    setCount((count)=> count +1)
+    setItem((prev)=>{
+      return [...prev, {
+        id : count,
+        text : input
+      }]
+    })
+    setInput("")
+  }
   return (
     <>
       <div className='container'>
@@ -13,10 +27,20 @@ function App() {
           <h1>To-Do List</h1>
         </div>
         <div className= "form">
-          <input type="text"/>
-          <button>
+          <input type="text" value={input} onChange={inputValue}/>
+          <button onClick={addItem}>
             <span>Add</span>
-          </button>
+          </button> 
+        </div>
+        <div>
+          <ul>
+          {item.map((item)=>{
+            return(<ListItem 
+              id = {item.id}
+              text = {item.text}
+            />)
+          })}  
+          </ul>
         </div>
       </div>
     </>
