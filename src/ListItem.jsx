aa1be001a0a0ac655402099ddf71377a232d5548
Md Faizuddin <imdfaizuddin@gmail.com>
2024-04-05 15:59:40 +0530

    add delete list func

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function ListItem(props) {
+    
     return (<li>
                 <span>{props.text}</span>
-                {props.del != null && 
-                <img onClick={() => props.del(props.id)} src="/delete.png" alt="delete btn" />}
+                {props.del != null ? 
+                <img onClick={() => props.del(props.id)} src="/delete.png" alt="delete btn" />:<span>{props.time}</span>}
+                
             </li>)
 }
 
